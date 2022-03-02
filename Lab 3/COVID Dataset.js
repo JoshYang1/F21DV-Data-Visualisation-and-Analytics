09f0e4bc2632c9db771c178a53292e6397665ce7
JoshYang1 <jy84@hw.ac.uk>
2022-03-02 15:47:27 +0000

    Created map visual

--- Lab 3/COVID Dataset.js
+++ Lab 3/COVID Dataset.js
@@ -2,25 +2,46 @@
     // importing the csv file
     let dataset = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv';
 
-    // var margin = {top: 20, right: 10, bottom: 40, left: 100},
-    // width = 600 - margin.left - margin.right,
-    // height = 400 - margin.top - margin.bottom;
+    var margin = {top: 20, right: 10, bottom: 40, left: 100},
+    width = 800 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-    // var svg = d3.select("svg")
-    //             .attr("width", width + margin.left + margin.right)
-    //             .attr("height", height + margin.top + margin.bottom)
-    //             .append("g")
-    //             .attr("transform",
-    //                     "translate(" + margin.left + "," + margin.top + ")");
+    // Have to use a SVG rather than a canvas as the latter does not support event handling
+    var svg = d3.select("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform",
+                        "translate(" + margin.left + "," + margin.top + ")");
+    
+    var projection = d3.geoNaturalEarth1()
+                        .scale(width / 1.5 / Math.PI)
+                        .translate([width / 2, height / 1.4])
+                        
+    var domain = [0, 500000000]
+    var labels = ["< 100 M", "100 M - 500 M", "> 500 M"]
+    var range = ["#F8CAEE","#BF76AF","#852170"]
+    var colorScale = d3.scaleThreshold()
+        .domain(domain)
+        .range(range);
 
-    // var projection = d3.geoMercator()
-    //                     .scale(70)
-    //                     .center([0,20])
-    //                     .translate([width / 2 - margin.left, height / 2]);
+    // https://www.d3-graph-gallery.com/graph/backgroundmap_basic.html
+    d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then(data => {
+
+            // Draw the map
+            svg.append("g")
+                .selectAll("path")
+                .data(data.features)
+                .join('path')
+                .attr("fill", "#69b3a2")
+                .attr("d", d3.geoPath()
+                .projection(projection)
+                )
+                .style("stroke", "#fff")
+    });
 
     // reading the csv file
     d3.csv(dataset).then(function(data) {
-
 
         // https://observablehq.com/@d3/d3-group-d3-hierarchy
         // https://observablehq.com/@d3/d3-hierarchy
@@ -29,10 +50,12 @@
         var root = d3.hierarchy(entries)
 
         // Hierarchy height is 4 so to access lowest level data will need 4 children (children[0].children[0].children[0].children[0])
+        // if date = latest date then store the results or whack it straight into d3
+        root.each((d[0]) => console.log(d.children[0]));
 
-        console.log(strat instanceof d3.hierarchy)
 
 
+    
 
 
         
