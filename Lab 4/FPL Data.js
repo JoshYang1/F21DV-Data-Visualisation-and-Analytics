313f3ba57d6523bb12654fe4e76c022488c522fe
JoshYang1 <jy84@hw.ac.uk>
2022-03-24 21:24:47 +0000

    v2

--- Lab 4/FPL Data.js
+++ Lab 4/FPL Data.js
@@ -3,18 +3,15 @@ const spreadsheet = "1_rkHKgIPt3i_2uKr8kh5u4WZaYOLccNiiTx1xAaAo_Y";
 const tab = 'Data';
 const endpoint1 = `${googleAPI}${spreadsheet}/${tab}`;
 
-var fplData;
+var fplData = fetchData(endpoint1);
 
-//https://benborgers.com/posts/google-sheets-json
-fetch(endpoint1)
-    .then((res) => res.json())
-    .then((data) => {
-        fplData = data
-
-      data.forEach((row) => {
-
-      });
-    });
+// https://dmitripavlutin.com/javascript-fetch-async-await/
+// https://benborgers.com/posts/google-sheets-json
+async function fetchData(endpoint){
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    return data;
+};
 
 
 // function to retrieve an element and add an event listener
---
+++ Lab 4/Scatter Plot.js
@@ -0,0 +1,68 @@
+const height = d3.select('#scatterPlot').style('height')
+const width = d3.select('#scatterPlot').style('width')
+
+const svg = d3.select("#scatterPlot")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .append("g")
+                .attr("transform", `translate(0,0)`)
+
+fplData.then(function(data) {
+
+    const scatterData = [];
+    
+    
+    data.forEach(function(d) {
+        var cost = (parseInt(d["Cost Today"]) / 10) || 0;
+        var ict = parseFloat(d["ICT Index"]) || 0;
+        scatterData.push({cost: cost, ict: ict});
+    })
+
+    console.log(scatterData)
+
+    // Add X axis
+    var x = d3.scaleLinear()
+                .domain([0, 0])
+                .range([ 0, width]);
+
+    svg.append("g")
+        .attr("class", "myXaxis")   // Note that here we give a class to the X axis, to be able to call it later and modify it
+        .attr("transform", "translate(0,0)")
+        .call(d3.axisBottom(x))
+        .attr("opacity", "0")
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+        .domain([0, 500000])
+        .range([ height, 0]);
+
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    // Add dots
+    svg.append('g')
+        .selectAll("dot")
+        .data([scatterData])
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) { return x(d.ict); } )
+        .attr("cy", function (d) { return y(d.cost); } )
+        .attr("r", 1.5)
+        .style("fill", "#69b3a2")
+
+    // new X axis
+    x.domain([0, 4000])
+    svg.select(".myXaxis")
+    .transition()
+    .duration(2000)
+    .attr("opacity", "1")
+    .call(d3.axisBottom(x));
+
+    svg.selectAll("circle")
+    .transition()
+    .delay(function(d,i){return(i*3)})
+    .duration(2000)
+    .attr("cx", function (d) { return x(d.ict); } )
+    .attr("cy", function (d) { return y(d.cost); } )
+})
